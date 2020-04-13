f5def55a539346e45b0f911d733648bda4f5f7ec
nextheroes <63160418+nextheroes@users.noreply.github.com>
2020-04-14 00:25:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -211,101 +211,76 @@ async def on_message(message):
         embed.set_footer(text = "Source - NextHeroes\nLv2 S2 KartRiderClub NextLv's Bot")
         await message.channel.send(embed=embed)
     
-
 ############카트############
     if message.content.startswith("!nh카트"):
-        response = requests.get('http://kart.nexon.com/Garage/Main?strRiderID='+message.content[6:])
-        response2 = requests.get('http://kart.nexon.com/Garage/Record?strRiderID='+message.content[6:])
-        response3 = requests.get('https://tmi.nexon.com/kart/user?nick='+message.content[6:])
-
-
+        response = requests.get('http://kart.nexon.com/Garage/Main?strRiderID='+message.content[5:])
+        response2 = requests.get('http://kart.nexon.com/Garage/Record?strRiderID='+message.content[5:])
+        
         readerhtml = response.text
         readerhtml2 = response2.text
-        readerhtml3 = response3.text
-
+        
         soup = BeautifulSoup(readerhtml, 'lxml')
         soup2 = BeautifulSoup(readerhtml2, 'lxml')
-        soup3 = BeautifulSoup(readerhtml3, 'lxml')
-        
-
-
 
         #차고1#
-        nick = soup.find('span', {'id' : 'RiderName'}).text
-        club = soup.find('span', {'id' : 'GuildName'}).text
-        rprank = soup.find('span',{'class' : 'RecordData1'}).text
-        rp = soup.find('span',{'class' : 'RecordData2'}).text
-        avatar = soup.find('div', {'id' : 'CharInfo'})
-        avatar2 = avatar.find('img').get('src')
+        nick = soup.find('span', {'id' : 'RiderName'}).text #닉네임
+        club = soup.find('span', {'id' : 'GuildName'}).text #클럽
+        rprank = soup.find('span',{'class' : 'RecordData1'}).text #RP 순위
+        rp = soup.find('span',{'class' : 'RecordData2'}).text #RP
+        avatar = soup.find('div', {'id' : 'CharInfo'}) #avatar.png
+        avatar2 = avatar.find('img').get('src') #avatar.png표시
         
         #차고2#
-        cnt = soup2.find('div', {'id' : 'CntRecord2'})
-        dlfind = cnt.findAll('dl')
-        starty = dlfind[0].find('dd').text[0:4]
-        startm = dlfind[0].find('dd').text[5:7]
-        startd = dlfind[0].find('dd').text[8:10]
-        startday = dlfind[0].find('dd').text[11:]
-        racing = dlfind[1].find('dd').text
-        gameon = dlfind[2].find('dd').text
+        cnt = soup2.find('div', {'id' : 'CntRecord2'}) #차고 메인 전체 크롤링
+        dlfind = cnt.findAll('dl') #dl태그 찾기
+        starty = dlfind[0].find('dd').text[0:4] #게임시작 년
+        startm = dlfind[0].find('dd').text[5:7] #게임시작 월
+        startd = dlfind[0].find('dd').text[8:10] #게임시작 일
+        startday = dlfind[0].find('dd').text[11:] #게임 시작후 지금까지 일
+        racing = dlfind[1].find('dd').text #게임시간
+        gameon = dlfind[2].find('dd').text #게임 실행
+        recenty = dlfind[3].find('dd').text[0:4] #최근 실행 년
+        recentm = dlfind[3].find('dd').text[5:7] #최근 실행 월
+        recentd = dlfind[3].find('dd').text[8:10] #최근실행 일
 
-        recenty = dlfind[3].find('dd').text[0:4]
-        recentm = dlfind[3].find('dd').text[5:7]
-        recentd = dlfind[3].find('dd').text[8:10]
-
-        
         #전체 승률#
-        recorddata2 = soup2.find('div', {'id' : 'CntRecord'})
-        allwinrate = recorddata2.find('td',{'class' : 'RecordL2'}).text[0:3]
-        allwin = recorddata2.find('td',{'class' : 'RecordL2'}).text[4:]
-        allwinrp = recorddata2.find('td',{'class' : 'RecordL3'}).text
+        recorddata2 = soup2.find('div', {'id' : 'CntRecord'}) #승률창 크롤링
+        allwinrate = recorddata2.find('td',{'class' : 'RecordL2'}).text[0:3] #전체승률 %
+        allwin = recorddata2.find('td',{'class' : 'RecordL2'}).text[4:] #전체 전적
+        allwinrp = recorddata2.find('td',{'class' : 'RecordL3'}).text #전체 RP 랭킹
         
         #스피드#
-        winrate = recorddata2.find('table', {'class' : 'RecordL'})
-        sprate = winrate.findAll('td')
-        spallrt = sprate[4].text[0:3]
-        spallrt2 = sprate[4].text[4:]
-        sprprank = sprate[5].text
+        winrate = recorddata2.find('table', {'class' : 'RecordL'}) #스피드 크롤링
+        sprate = winrate.findAll('td') #스피드전적창에서 td찾기
+        spallrt = sprate[4].text[0:3] #스피드 전체 %
+        spallrt2 = sprate[4].text[4:] #스피드 전체 전적
+        sprprank = sprate[5].text #스피드 RP 랭킹
         
         #아이템#
-        iprallrt = sprate[7].text[0:3]
-        iprallrt2 = sprate[7].text[4:]
-        iprprank = sprate[8].text
+        iprallrt = sprate[7].text[0:3] #스피드 크롤링과 같은 클래스 아이템 전체 %
+        iprallrt2 = sprate[7].text[4:] #아이템 전체 전적
+        iprprank = sprate[8].text #아이템 RP 랭킹
         
-        kartembed = discord.Embed(color=0x900020)
-        if soup3.find('span', {'class' : 'license l1'}):
-            kartembed.set_author(name= nick, icon_url='https://tmi.nexon.com/img/icon_l1.png')
-        elif soup3.find('span',{'class' : 'license l2'}):
-            kartembed.set_author(name= nick, icon_url='https://tmi.nexon.com/img/icon_l2.png')
-        elif soup3.find('span',{'class' : 'license l3'}):
-            kartembed.set_author(name= nick, icon_url='https://tmi.nexon.com/img/icon_l3.png')
-        elif soup3.find('span',{'class' : 'license rookie'}):
-            kartembed.set_author(name= nick, icon_url='https://tmi.nexon.com/img/icon_rookie.png')
-        elif soup3.find('span',{'class' : 'license beginner'}):
-            kartembed.set_author(name= nick, icon_url='https://tmi.nexon.com/img/icon_beginner.png')
-        elif soup3.find('span',{'class' : 'license pro'}):
-            kartembed.set_author(name= nick, icon_url='https://tmi.nexon.com/img/icon_pro.png')
-        else:
-            kartembed.set_author(name= nick)
-
-        kartembed.add_field(name = "Club", value = club, inline = True)
-        kartembed.add_field(name = "RP", value = rprank + "\n" + rp, inline = True)
-        kartembed.add_field(name = "All Win Rate", value = allwinrate + "\n" + "(" + allwin + ")", inline = True)
-        kartembed.add_field(name = "Speed Win Rate", value = spallrt + "\n" + "(" + spallrt2 + ")", inline = True)
-        kartembed.add_field(name = "Item Win Rate", value = iprallrt + "\n" + "(" + iprallrt2 + ")", inline = True)
-        kartembed.add_field(name = "All RP", value = allwinrp, inline = True)
-        kartembed.add_field(name = "Speed RP", value = sprprank, inline = True)
-        kartembed.add_field(name = "Item RP", value = iprprank, inline = True)
+        #출력#
+        kartembed = discord.Embed(color=0x900020, title = message.content[5:]) #버건디 컬러 embed + 닉네임
+        kartembed.add_field(name = "NickName", value = nick, inline = True) #닉네임 출력
+        kartembed.add_field(name = "Club", value = club, inline = True) #클럽 출력
+        kartembed.add_field(name = "RP", value = rprank + "\n" + rp, inline = True) #RP순위와 RP출력
+        kartembed.add_field(name = "All Win Rate", value = allwinrate + "\n" + "(" + allwin + ")", inline = True) #전체승률 출력
+        kartembed.add_field(name = "Speed Win Rate", value = spallrt + "\n" + "(" + spallrt2 + ")", inline = True) #스피드 승률 출력
+        kartembed.add_field(name = "Item Win Rate", value = iprallrt + "\n" + "(" + iprallrt2 + ")", inline = True) #아이템 승률 출력
+        kartembed.add_field(name = "All RP", value = allwinrp, inline = True) #전체 RP 출력
+        kartembed.add_field(name = "Speed RP", value = sprprank, inline = True) #스피드 RP 출력
+        kartembed.add_field(name = "Item RP", value = iprprank, inline = True) #아이템 RP 출력
         kartembed.add_field(name = "Rider Creation", value = f'{starty}년 '+f'{startm}월 '+f'{startd}일' "\n" + startday, inline = True)
-        kartembed.add_field(name = "Driving Time", value = racing, inline = True)
-        kartembed.add_field(name = "Game Runs", value = gameon, inline = True)
-        kartembed.add_field(name = "Recent Access", value = f'{recenty}년 '+f'{recentm}월 '+f'{recentd}일')
-        kartembed.add_field(name = "3렙 터졌다!!!!", value = "3렙 도와주실 분을 찾습니다...\n카카오톡 qkqkhih......")
-        kartembed.add_field(name="TMI",value=f'[KartRiderTMI](https://tmi.nexon.com/kart/user?nick={nick})')
-        kartembed.set_footer(text="Source - NextHeroes\nLv2 S2 KartRiderClub NextLv's Bot\n3렙 도와주실분들을 찾습니다..카카오톡 qkqkhih...")
-        kartembed.set_thumbnail(url = avatar2)
-        await message.channel.send(embed=kartembed)
-
-
+        #게임시작일 출력
+        kartembed.add_field(name = "Driving Time", value = racing, inline = True) #주행시간 출력
+        kartembed.add_field(name = "Game Runs", value = gameon, inline = True) #게임 실행 횟수 출력
+        kartembed.add_field(name = "Recent Access", value = f'{recenty}년 '+f'{recentm}월 '+f'{recentd}일') #게임 최근 접속일 출력
+        kartembed.add_field(name="TMI",value=f'[KartRiderTMI](https://tmi.nexon.com/kart/user?nick={nick})') #카트라이더 TMI 연결
+        kartembed.set_footer(text="Source - NextHeroes\nLv2 S2 KartRiderClub NextLv's Bot") #만든 사람
+        embed.set_thumbnail(url = avatar2) #avatar.png 출력
+        await message.channel.send(embed=kartembed) #embed
 ############롤 솔랭############        
     if message.content.startswith("!nh롤솔랭"):
         response = requests.get('https://www.op.gg/summoner/userName='+message.content[7:]) #롤 전적사이트 op.gg 링크
